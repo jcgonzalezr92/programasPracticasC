269557d5e321dcea0a128c5163fd3aff88c9c5d0
julio cesar <jc.gonzalez.r92@gmail.com>
2014-10-08 21:45:05 -0500

    sin comentario

--- practica1a/codigo.c
+++ practica1a/codigo.c
@@ -7,6 +7,7 @@
 #use fast_io(c)
 #use fast_io(e)  
 #use rs232(baud=9600, xmit=pin_c6,rcv=pin_c7, parity=N, bits=8)
+#use pwm(CCP1,TIMER=2,FREQUENCY=38000,DUTY=10)
 
 int estadoSensor = 0;
 float potenciometro;
@@ -32,14 +33,18 @@ timer=0;
 void interrupcionSensor(){
 //tadoSensor=input(PIN_B1);
 if (input(PIN_B4)==1){
-output_low(PIN_B1);
-printf("Desbloqueado %Ld \n", segundo);
-segundo=0;
+	output_low(PIN_B1);
+	if (segundo != 0){
+		printf("Bloqueado %Ld Segundos \n\r ", segundo);
+		segundo=0;
+	} 
 }
 else if(input(PIN_B4)==0){
-output_high(PIN_B1);
-printf("Bloqueado %Ld \n", segundo);
-segundo=0;
+	output_high(PIN_B1);
+	if (segundo != 0){
+		printf("Desbloqueado %Ld Segundos \n\r ", segundo);
+		segundo=0;
+	}
 }
 }
 void main(){
@@ -54,9 +59,9 @@ void main(){
 	ENABLE_INTERRUPTS(GLOBAL);
 	SETUP_ADC_PORTS(RA0_ANALOG);
 	setup_adc(ADC_CLOCK_DIV_32);
-	setup_ccp2(CCP_PWM);
-	setup_timer_2(T2_DIV_BY_1,25,1);			//prescales de 1, valor timer2 =25                       	
-	set_pwm2_duty(0.000000526);
+	//setup_ccp1(CCP_PWM);
+	//setup_timer_2(T2_DIV_BY_1,25,1);			//prescales de 1, valor timer2 =25                       	
+	//set_pwm2_duty(0.000000526);
 	setup_timer_0(RTCC_INTERNAL | RTCC_DIV_16);
 	set_timer0(6);
 while (1){
@@ -66,6 +71,7 @@ while (1){
 	voltaje=potenciometro*5.0/1023.0;
 	valorPwm=voltaje*200.0;
 	//set_pwm2_duty(valorPwm);
+    pwm_set_duty_percent(valorPwm);
 	output_toggle(PIN_B2);
 	delay_ms(500);
 	
